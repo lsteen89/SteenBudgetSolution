7fe4155874aea00e5071bb55085ae7b1e5c4562e
Linus Steen <linus.j.steen@gmail.com>
2024-03-17 18:22:01 +0100

    Fixed logo

--- Frontend/src/components/HomePage.js
+++ Frontend/src/components/HomePage.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './HomePage.css';
-import MainPageRectSVG from '../assets/Images/MainPageRect.svg';
+import MainPageBird from '../assets/Images/MainPageBird.svg';
 
 const HomePage = () => {
   return (
-    <div className="containerStyle">
-      <p>
-      Ta kontroll över din ekonomi och skapa trygghet för framtiden genom smart budgetering!
-      </p>
+    <div className="MainPageWrapper">
+      <div className="MainPageTextContainer">
+        <p>
+          Ta kontroll över din ekonomi och skapa trygghet för framtiden genom smart budgetering!
+        </p>
+      </div>
+      <button className="Skaffa-Ebudget">Skaffa eBudget</button>
+      <img src={MainPageBird} alt="Bird" style={{ marginTop: '10vh' }} />
     </div>
   );
 };
-
 
 export default HomePage;
