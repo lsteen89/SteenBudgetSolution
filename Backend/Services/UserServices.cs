b4a2af4b0a6a3de9584ef213ec514a659a27a37f
lsteen89 <linus.j.steen@gmail.com>
2024-06-17 18:44:58 +0200

    Added JWT support, changed my github action

--- Backend/Services/UserServices.cs
+++ Backend/Services/UserServices.cs
@@ -1,15 +1,21 @@
 ﻿using Backend.DataAccess;
 using Backend.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Backend.Services
 {
     public class UserServices
     {
         private readonly SqlExecutor _sqlExecutor;
+        private readonly string _jwtSecretKey;
 
-        public UserServices(SqlExecutor sqlExecutor)
+        public UserServices(SqlExecutor sqlExecutor, IConfiguration configuration)
         {
             _sqlExecutor = sqlExecutor;
+            _jwtSecretKey = configuration["JwtSecretKey"];
         }
 
         public bool CheckIfUserExists(string email)
@@ -20,6 +26,25 @@ namespace Backend.Services
         public bool CreateNewRegisteredUser(UserModel user)
         {
             return _sqlExecutor.InsertNewUserDatabase(user);
+        }
+        public string GenerateJwtToken(UserModel user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(_jwtSecretKey);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                new Claim("id", user.Id.ToString()),
+                new Claim("email", user.Email)
+            }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
     }
 }
