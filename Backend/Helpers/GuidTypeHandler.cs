4e8e56d2e3568024732f89cc638a1e8563888c19
lsteen89 <linus.j.steen@gmail.com>
2024-10-10 07:47:13 +0200

    Refactor: Change PersoId type from string to Guid, add Guid to string handler, complete Verify Email API, and add Create User script - Changed internal PersoId type from string to Guid for consistency - Added Dapper type handler for Guid to string conversion - Completed the logic for Verify Email API - Added SQL script to create a new user

---
+++ Backend/Helpers/GuidTypeHandler.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using System;
+using System.Data;
+
+namespace Backend.Helpers
+{
+    public class GuidTypeHandler : SqlMapper.TypeHandler<Guid>
+    {
+        // This method sets the value of the parameter when you're sending data to the database
+        public override void SetValue(IDbDataParameter parameter, Guid value)
+        {
+            // Convert the GUID to a string (since CHAR(36) is a string in the database)
+            parameter.Value = value.ToString();
+        }
+
+        // This method parses the value from the database (CHAR(36)) to a GUID when retrieving data
+        public override Guid Parse(object value)
+        {
+            // Convert the CHAR(36) string from the database to a Guid
+            return Guid.Parse(value.ToString());
+        }
+    }
+}
--- Backend/Models/UserExpenseRatioModel.cs
+++ Backend/Models/UserExpenseRatioModel.cs
@@ -6,10 +6,10 @@
         public Guid PersoId { get; set; }
         public Guid? PartnerId { get; set; }
         public decimal Ratio { get; set; }
-        public string CreatedBy { get; set; }
+        public string? CreatedBy { get; set; }
         public DateTime CreatedTime { get; set; }
         public DateTime? LastUpdatedTime { get; set; }
-        public UserModel User { get; set; }
+        public UserModel? User { get; set; }
     }
 
 }
