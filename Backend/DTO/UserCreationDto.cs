46a7eb251277a788d9a8ac5c9e87d8df863c4bcb
lsteen89 <linus.j.steen@gmail.com>
2024-10-10 07:59:41 +0200

    Handle of various null checks, some suppressed, some handled.

--- Backend/DTO/UserCreationDto.cs
+++ Backend/DTO/UserCreationDto.cs
@@ -7,21 +7,21 @@ namespace Backend.DTO
         [Required(ErrorMessage = "First name is required.")]
         [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         [RegularExpression(@"^[\p{L}]+(([',. -][\p{L} ])?[\p{L}]*)*$", ErrorMessage = "Invalid first name format.")]
-        public string FirstName { get; set; }
+        public string? FirstName { get; set; }
 
         [Required(ErrorMessage = "Last name is required.")]
         [StringLength(50, ErrorMessage = "Last name cannot be longer than 50 characters.")]
         [RegularExpression(@"^[\p{L}]+(([',. -][\p{L} ])?[\p{L}]*)*$", ErrorMessage = "Invalid last name format.")]
-        public string LastName { get; set; }
+        public string? LastName { get; set; }
 
         [Required(ErrorMessage = "Email is required.")]
         [StringLength(100, ErrorMessage = "E-mail cannot be longer than 100 characters.")]
         [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage = "Invalid email format")]
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
         [StringLength(100, ErrorMessage = "Password cannot be longer than 100 characters.")]
         [RegularExpression(@"^(?=.*[A-Z])(?=.*\d)(?=.*[\W]).{8,}$", ErrorMessage = "Password must be at least 8 characters long and include at least one uppercase letter, one number, and one symbol.")]
-        public string Password { get; set; }
+        public string? Password { get; set; }
     }
 }
