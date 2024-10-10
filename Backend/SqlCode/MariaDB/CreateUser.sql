93e1bd20cb2a2465865b463344c5b48565184c76
lsteen89 <linus.j.steen@gmail.com>
2024-10-10 07:47:13 +0200

    Refactor: Change PersoId type from string to Guid, add Guid to string handler, complete Verify Email API, and add Create User script - Changed internal PersoId type from string to Guid for consistency - Added Dapper type handler for Guid to string conversion - Completed the logic for Verify Email API - Added SQL script to create a new user

--- Backend/SqlCode/MariaDB/CreateUser.sql
+++ Backend/SqlCode/MariaDB/CreateUser.sql
@@ -1,10 +1,8 @@
---Prod user for the SteenBudgetSystem database
+-- Prod user for the SteenBudgetSystem database
 GRANT ALL PRIVILEGES ON SteenBudgetSystemPROD.* TO 'admin'@'192.168.50.%' IDENTIFIED BY 'your_admin_password';
 FLUSH PRIVILEGES;
 
---Test user for the SteenBudgetSystem database
+-- Test user for the SteenBudgetSystem database
 CREATE USER 'test_user'@'192.168.50.18' IDENTIFIED BY 'ffff';
-
-GRANT SELECT, INSERT, UPDATE, DELETE ON SteenBudgetSystemTEST.* TO 'test_user'@'192.168.50.18';
-
+GRANT ALL PRIVILEGES ON steenbudgetsystemtest.* TO 'test_user'@'192.168.50.18' IDENTIFIED BY 'ffff';
 FLUSH PRIVILEGES;
