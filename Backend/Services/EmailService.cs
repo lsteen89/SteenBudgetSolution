a7ee2786542ea1c5a210390595d0082da02da8b3
lsteen89 <linus.j.steen@gmail.com>
2024-10-15 14:42:05 +0200

    Addded log

--- Backend/Services/EmailService.cs
+++ Backend/Services/EmailService.cs
@@ -46,7 +46,8 @@ public class EmailService : IEmailService
                     // Fetch the SMTP password from the environment variable (if set) or fallback to the config file
                     var smtpPassword = Environment.GetEnvironmentVariable("SMTP_PASSWORD")
                         ?? _configuration["Smtp:Password"];
-                    _logger.LogInformation("SMTP password: {Password}", smtpPassword); // For debugging
+                    _logger.LogInformation("SMTP_PASSWORD from environment: {Password}", Environment.GetEnvironmentVariable("SMTP_PASSWORD"));
+
 
                     // Authenticate
                     _logger.LogInformation("Authenticating as {Username}", _configuration["Smtp:Username"]);
