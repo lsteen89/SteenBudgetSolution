05283bf92974d2a772946b20a1a129a595b105f4
lsteen89 <linus.j.steen@gmail.com>
2025-01-19 15:10:48 +0100

    Edit the DeepBlueContainer, make current behavior (rounded corners) default, add option to have no corners. Adjust deepbluecontainer on homepage, making only top right and top left corner rounded.

--- Frontend/src/components/molecules/containers/DeepBlueContainer.tsx
+++ Frontend/src/components/molecules/containers/DeepBlueContainer.tsx
@@ -3,17 +3,20 @@ import React from 'react';
 interface DeepBlueContainerProps {
   children: React.ReactNode;
   additionalClasses?: string; // Allows custom classes for flexibility
+  noRoundedCorners?: boolean; // Suppresses the default rounded-lg
 }
 
 const DeepBlueContainer: React.FC<DeepBlueContainerProps> = ({
   children,
   additionalClasses = '',
+  noRoundedCorners = false, // Default: rounded corners are applied
 }) => {
   return (
     <div
       className={`
         bg-[#001F3F] bg-[url('../../assets/Components/Shapes/MainPageRect.png')] bg-cover bg-center 
-        overflow-hidden shadow-md rounded-lg 
+        overflow-hidden shadow-md 
+        ${noRoundedCorners ? '' : 'rounded-lg'} 
         ${additionalClasses}
       `}
     >
