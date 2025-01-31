96a7b89d610282aaafa80d028b65da618486b2fd
lsteen89 <linus.j.steen@gmail.com>
2025-01-31 08:18:29 +0100

    Adjusted protected route

--- Frontend/src/routes/ProtectedRoute.tsx
+++ Frontend/src/routes/ProtectedRoute.tsx
@@ -1,21 +1,26 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation  } from "react-router-dom";
 import { useAuth } from "@context/AuthProvider";
+
+interface ProtectedRouteProps {
+  children: JSX.Element;
+}
 
 const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const auth = useAuth();
   const isDebugMode = process.env.NODE_ENV === 'development';
+  const location = useLocation();
 
   if (isDebugMode) {
     console.log("Debug mode active: bypassing authentication");
     return <>{children}</>; // Bypass authentication in development
   }
 
-  if (auth.authenticated) {
+  if (auth?.authenticated) {
     return <>{children}</>;
   }
 
-  return <Navigate to="/login" />;
+  return <Navigate to="/login" state={{ from: location }} replace />;
 };
 
 export default ProtectedRoute;
