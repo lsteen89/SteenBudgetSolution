09c140c119a297f0bdbd39ad4c05c4d506bf0a1e
lsteen89 <linus.j.steen@gmail.com>
2025-02-17 17:15:51 +0100

    feat(backend/setup-wizard): Returning FirstTimeLogin in the auth endpoint from the backend to let the frontend know if the user needs setup or not. Added new method to help the controller, NeedsInitialSetupAsync, in userManagementService. Added FirstTimeLogin to AuthStatusDto

--- Backend/Application/DTO/AuthStatusDto.cs
+++ Backend/Application/DTO/AuthStatusDto.cs
@@ -10,5 +10,6 @@ namespace Backend.Application.DTO
         public string Email { get; set; }
         [JsonPropertyName("role")]
         public string Role { get; set; }
+        public bool FirstTimeLogin { get; set; }
     }
 }
