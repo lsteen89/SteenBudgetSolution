c1c461fd307b8f3515415fd52c0967436e649bd2
lsteen89 <linus.j.steen@gmail.com>
2025-03-24 14:37:15 +0100

    Changed log entries to debug mode in order to not spam prod logs

--- Backend/Application/Services/UserServices/UserAuthenticationService.cs
+++ Backend/Application/Services/UserServices/UserAuthenticationService.cs
@@ -202,7 +202,7 @@ namespace Backend.Application.Services.UserServices
             string? email = user.GetEmail();
             var role = user.FindFirst("role")?.Value;
             string? jti = user.FindFirst(JwtRegisteredClaimNames.Jti)?.Value;
-            _logger.LogInformation("DEBUG REMOVE LATER: JTI: {Jti}", jti); // Log for debugging
+            _logger.LogDebug("DEBUG REMOVE LATER: JTI: {Jti}", jti); // Log for debugging
 
             if (string.IsNullOrEmpty(jti))
             {
