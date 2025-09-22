6b4ac43dc1247ba5ec63c0aeae78c61417fc2f5b
Lsteen89 <linus.j.steen@gmail.com>
2025-09-22 06:58:32 +0200

    Meaningless commit to troubleshoot

--- Frontend/src/Pages/Home/DesktopHomePage.tsx
+++ Frontend/src/Pages/Home/DesktopHomePage.tsx
@@ -4,7 +4,7 @@ import MainPageBird from '@assets/Images/MainPageBird.png';
 import SubmitButton from '@components/atoms/buttons/SubmitButton';
 import useDisableScroll from '@hooks/useDisableScroll';
 import { BookOpenIcon, ChartBarIcon, ShieldCheckIcon } from '@heroicons/react/24/outline';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
 import DeepBlueContainer from '@components/molecules/containers/DeepBlueContainer';
 import '@styles/animations.css';
 
@@ -18,8 +18,8 @@ const HomePage: React.FC = () => {
 
   return (
     <div className="relative align-items: flex-start flex items-center justify-center h-screen gap-[8vw] mt-[15vh]">
-        {/* Left Container */}
-        <DeepBlueContainer noRoundedCorners additionalClasses="h-[70vh] flex-grow flex-col flex items-center max-w-md space-y-10 mt-[10%] px-8 h-full pt-[2%]
+      {/* Left Container */}
+      <DeepBlueContainer noRoundedCorners additionalClasses="h-[70vh] flex-grow flex-col flex items-center max-w-md space-y-10 mt-[10%] px-8 h-full pt-[2%]
         lg:mt-[15%] rounded-tl-2xl rounded-tr-2xl
         ">
         <p className="text-white text-center m-0 tracking-[0.2em] max-w-[90%] text-[clamp(1rem,2vw,1.5rem)] leading-[clamp(1.5,2.5vw,2)]">
@@ -31,7 +31,7 @@ const HomePage: React.FC = () => {
         <div className="pt-14 flex flex-col space-y-8 items-center">
           <div className="relative group flex flex-col items-center">
             <BookOpenIcon className="h-10 w-10 text-limeGreen transition-transform transform group-hover:scale-110 group-hover:shadow-lg" />
-            <p className="text-white mt-2 text-center">Enkel planering</p>
+            <p className="text-white mt-2 text-center">Enkel planering!</p>
             {/* Tooltip */}
             <div className="absolute bottom-14 hidden group-hover:block bg-black text-white text-xs p-2 rounded shadow-lg">
               Planera din budget enkelt och snabbt! Läs mer under<Link to="/" className="underline text-limeGreen"> Hjälp </Link>
@@ -54,27 +54,27 @@ const HomePage: React.FC = () => {
             </div>
           </div>
         </div>
-        </DeepBlueContainer>
-        
-        {/* Right-Side Container */}
-        <div className="flex flex-col justify-center space-y-8 h-full         
+      </DeepBlueContainer>
+
+      {/* Right-Side Container */}
+      <div className="flex flex-col justify-center space-y-8 h-full         
             lg:mt-[5%]
           ">
-          <SubmitButton
-            isSubmitting={false}
-            label="Skaffa eBudget!"
-            size="large"
-            enhanceOnHover
-            onClick={handleRedirect}
-          />
-          <img
-            src={MainPageBird}
-            alt="Main Page Bird"
-            className="max-w-full h-auto img-float hover:animate-img-flap"
-          />
+        <SubmitButton
+          isSubmitting={false}
+          label="Skaffa eBudget!"
+          size="large"
+          enhanceOnHover
+          onClick={handleRedirect}
+        />
+        <img
+          src={MainPageBird}
+          alt="Main Page Bird"
+          className="max-w-full h-auto img-float hover:animate-img-flap"
+        />
 
-        </div>
-        
+      </div>
+
     </div>
   );
 };
